d88f51e10a279e2d0e9353f473f711ac0bda80cf
Cry4me1 <ddanthumytrang@gmail.com>
2026-03-01 19:18:05 +0800

    vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-export const runtime = "edge";
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
